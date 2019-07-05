16c655cbc5b4fb94d555236e892a18f862ce8d4d
Alex Merenda <alex.merenda@impact.com>
2019-07-05 12:01:29 -0400

    removes nfs-hdd from install

--- 2-NFS_Storage/deploy_nfs_storage.sh
+++ 2-NFS_Storage/deploy_nfs_storage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 kubectl apply -f 1-namespace.yaml          
 kubectl apply -f 2-rbac.yaml               
-kubectl apply -f 3-deployment-arm-hd.yaml  
+#kubectl apply -f 3-deployment-arm-hd.yaml  
 kubectl apply -f 4-deployment-arm-ssd.yaml 
-kubectl apply -f 5-storageclass-hd.yaml
+#kubectl apply -f 5-storageclass-hd.yaml
 kubectl apply -f 6-storageclass-ssd.yaml
 
 kubectl patch storageclass nfs-ssd1 -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
