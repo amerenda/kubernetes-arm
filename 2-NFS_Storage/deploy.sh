a487d17cd73ad3adb7077dd05fdbb1577c35a0d1
Alex Merenda <alex.merenda@impact.com>
2019-07-14 22:43:41 -0400

    cleans up whitespace

--- 2-NFS_Storage/deploy.sh
+++ 2-NFS_Storage/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-kubectl apply -f 1-namespace.yaml          
-kubectl apply -f 2-rbac.yaml               
-#kubectl apply -f 3-deployment-arm-hd.yaml  
-kubectl apply -f 4-deployment-arm-ssd.yaml 
+kubectl apply -f 1-namespace.yaml
+kubectl apply -f 2-rbac.yaml
+#kubectl apply -f 3-deployment-arm-hd.yaml
+kubectl apply -f 4-deployment-arm-ssd.yaml
 #kubectl apply -f 5-storageclass-hd.yaml
 kubectl apply -f 6-storageclass-ssd.yaml
 
